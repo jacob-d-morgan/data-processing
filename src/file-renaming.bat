3033cb040e89515d50e4bcbf3f6d860be555a999
jacob-d-morgan <48489389+jacob-d-morgan@users.noreply.github.com>
2019-05-06 15:42:00 -0700

    Add description of function to file-renaming.bat

---
+++ src/file-renaming.bat
@@ -0,0 +1,4 @@
+rem file-renaming.bat
+rem =================
+rem Recursively renames all the .caf and .did files in a directory.
+rem Prepends the date and time that they were created, and the name of the sub-directory they are in.
