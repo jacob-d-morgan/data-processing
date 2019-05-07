9d4866d2526668019091901f35469eab4e8c1043
jacob-d-morgan <48489389+jacob-d-morgan@users.noreply.github.com>
2019-05-07 16:22:35 -0700

    Define sections and add comments to code

--- src/prepend-date-created.bat
+++ src/prepend-date-created.bat
@@ -2,19 +2,35 @@
 @rem ========================
 @rem Recursively renames all the .caf and .did files in a folder.
 @rem Prepends the date that they were created, and the name of the sub-directory they are in.
+@rem ========================================================================================
+
+
+rem Preamble
+rem ========
+
+@echo off
+setlocal enableDelayedExpansion
 
 cls
 echo "File Dates" >fileDates.txt
 
-@echo off
-setlocal enableDelayedExpansion
+
+rem Define Working Directory
+rem ========================
 
 set "dirToUse=C:\Users\Jacob\Documents\projects\data-processing\data\xp\copy-of-results\Results\Test Folder\Another Test Folder\Kenji 15 N"
 
 set "getdate="
 
+
+rem Loop Through Files
+rem ==================
+
 for %%F in ("%dirToUse%\*.did") do (
 	
+	rem Get Date File Created
+	rem =====================
+
 	set "filename=%%F"
 	echo "File Name:">>fileDates.txt
 	echo !filename!>>fileDates.txt
@@ -38,6 +54,11 @@ for %%F in ("%dirToUse%\*.did") do (
 			echo "File Creation Date:">>fileDates.txt
 			echo !getdate!>>fileDates.txt
 			echo.>>fileDates.txt))
+
+
+	rem Get Relative Path to File
+	rem =========================
+
 
 echo "Working...")
 
