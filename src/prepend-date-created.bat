c9df320bdc0f86481cb4108d482dae8f01e54d2e
jacob-d-morgan <48489389+jacob-d-morgan@users.noreply.github.com>
2019-05-09 16:09:25 -0700

    Changed code to work with no backslash at the end of the root directory path

--- src/prepend-date-created.bat
+++ src/prepend-date-created.bat
@@ -17,30 +17,31 @@ cls
 rem Define Working Directory and Create Destination Directory
 rem =========================================================
 
-set "rootDir=C:\Users\Jacob\Documents\projects\data-processing\data\xp\copy-of-results\Results\"
+set "rootDir=C:\Users\Jacob\Documents\projects\data-processing\data\xp\copy-of-results\Results"
 cd %rootDir%
 
 for %%i in (.) do set "rootDirName=%%~nxi"
-mkdir "%rootDir%..\Processed-Files-from-%rootDirName%"
+mkdir "%rootDir%\..\Processed-Files-from-%rootDirName%"
 
 rem Loop Through Files
 rem ==================
 
-for /r "%rootDir%" %%F in ("*.did") do (
+for /r "%rootDir%\" %%F in ("*.did") do (
 
 	rem Get Relative Path to File
 	rem =========================
 	
+	rem Get the Path to the Directory from the File Path
 	set "absPath=%%~dpF"
 
-	for /l %%n in (1,1,500) do (if "!rootDir:~%%n,1!" neq "" (set /a "lenRootDir=%%n+1"))
+	rem Trim the trailing backslash from the Path to the Directory
 	for /l %%N in (1,1,500) do (if "!absPath:~%%N,1!" neq "" (set /a "lenAbsPath=%%N"))
 	for %%L in (!lenAbsPath!) do set "absPath=!absPath:~0,%%L!"
+	
+	rem Get the Relative Path from the Absolute Path
+	for /l %%n in (1,1,500) do (if "!rootDir:~%%n,1!" neq "" (set /a "lenRootDir=%%n+2"))
 	for %%l in (!lenRootDir!) do set "relPath=!absPath:~%%l!"
 
-	echo File Name: %%F
-	echo Abs Path: !absPath!
-	echo Rel Path: !relPath!
 
 	rem Get Date File Created
 	rem =====================
@@ -68,14 +69,13 @@ for /r "%rootDir%" %%F in ("*.did") do (
 	set "folderNames="
 	if "!relPath!" neq "" (
 		set "folderNames=!relPath:\=--!"
-		set "folderNames=!folderNames: =-!" 
+		set "folderNames=!folderNames: =-!"
 		)
 
 	set "newName=!getdate!_!folderNames!_%%~nxF"
+	copy "%%F" "!rootDir!\..\Processed-Files-from-!rootDirName!\!newName!"
 	
-	copy "%%F" "!rootdir!..\Processed-Files-from-!rootDirName!\!newName!"
-
 	echo Working: Copying files from !relPath!...
 	)
 
-echo "Completed"
+echo "Script Completed"
