21a5ed9bcaba852d087b226f346f9a0e4b82c48c
jacob-d-morgan <48489389+jacob-d-morgan@users.noreply.github.com>
2019-05-25 17:50:09 -0700

    Start the code to import a .csv file

--- src/dataImport.m
+++ src/dataImport.m
@@ -1,1 +1,17 @@
 %% dataImport %%
+
+% Import Data and Fix Variable Types
+%xp2018 = readtable('XP-2018(excelExportIntensityJDM).csv');
+xp2018.TimeCode = datenum(xp2018.TimeCode);
+xp2018.isRef = logical(xp2018.isRef);
+
+
+% Sort by Time Code
+idxTimeCode = find(cellfun(@(varName) strcmp('TimeCode',varName),xp2018.Properties.VariableNames)==1);
+xp2018 = sortrows(xp2018,idxTimeCode);
+
+
+% Split into Sample and Reference Measurements
+
+workingTable = xp2018_int(1:10000,:);
+
