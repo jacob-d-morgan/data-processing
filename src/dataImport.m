06b4f6fd4c6ea80a827c6c3e304d884347e2b29e
jacob-d-morgan <48489389+jacob-d-morgan@users.noreply.github.com>
2019-07-02 19:02:48 -0700

    Modify code to accept multiple reprocessed files

--- src/dataImport.m
+++ src/dataImport.m
@@ -1,13 +1,24 @@
 %% dataImport %%
 
 %% Import Data and Fix Variable Types
-xp2018 = readtable('XP-2018(excelExportIntensityJDM).csv');
-xp2018.TimeCode = datetime(datenum(xp2018.TimeCode),'ConvertFrom','Datenum'); %This is probably redundant...
-xp2018.Date = datestr(datenum(xp2018.Date) + datenum('31 Dec 1999')); %Correct for two-character month '0018'
-xp2018.IsRef__ = logical(xp2018.IsRef__);
+%xp2018 = readtable('XP-2018(excelExportIntensityJDM).csv','Delimiter',',');
+%disp({'Loading file 1...'})
+%xp2017 = readtable('XP-2017(excelExportIntensityJDM).csv','Delimiter',',');
+%disp({'Loading file 2...'})
+%xp2016 = readtable('XP-2016(excelExportIntensityJDM).csv','Delimiter',',');
+%disp({'Loading file 3...'})
+%%xp2015 = readtable('XP-2015(excelExportIntensityJDM).csv','Delimiter',',');
+%%
+xp2018.MeasurmentErrors = num2cell(xp2018.MeasurmentErrors);
+xp2016.MeasurmentErrors = num2cell(xp2016.MeasurmentErrors);
+importedData = [xp2018; xp2017; xp2016];
 
-idxTimeCode = find(cellfun(@(varName) strcmp('TimeCode',varName),xp2018.Properties.VariableNames)==1);
-xp2018 = sortrows(xp2018,idxTimeCode);
+importedData.TimeCode = datetime(datenum(importedData.TimeCode),'ConvertFrom','Datenum'); %This is probably redundant...
+importedData.Date = datestr(datenum(importedData.Date) + datenum('31 Dec 1999')); %Correct for two-character month '0018'
+importedData.IsRef__ = logical(importedData.IsRef__);
+
+idxTimeCode = find(cellfun(@(varName) strcmp('TimeCode',varName),importedData.Properties.VariableNames)==1);
+importedData = sortrows(importedData,idxTimeCode);
 
 %% Check the Gas Configuration and Gas Names
 % Do something here to highlight the different unique values of the Gas
@@ -17,7 +28,7 @@ xp2018 = sortrows(xp2018,idxTimeCode);
 %
 % Could do something similar here with folder names etc.
 
-% xp2018 = xp2018(contains(xp2018.FileHeader_Filename,'SPICE'),:);
+% importedData = importedData(contains(importedData.FileHeader_Filename,'SPICE'),:);
 
 %% Interpolate ST Voltages and Calculate Delta Values
 % NOTE: The current method of interpolating onto the ~isRef indices
@@ -26,10 +37,10 @@ xp2018 = sortrows(xp2018,idxTimeCode);
 % must remember to add them back in, including their metadata, which is
 % also excluded when I subsample the metadata columns for the ~isRef rows.
 
-intSA = xp2018(~xp2018.IsRef__,1:9);
-intST = array2table(interp1(find(xp2018.IsRef__), ...
-                    xp2018{xp2018.IsRef__,1:9}, ...
-                    find(~xp2018.IsRef__)));
+intSA = importedData(~importedData.IsRef__,1:9);
+intST = array2table(interp1(find(importedData.IsRef__), ...
+                    importedData{importedData.IsRef__,1:9}, ...
+                    find(~importedData.IsRef__)));
 intST.Properties.VariableNames = intSA.Properties.VariableNames;
 
 cycle_deltas = table();
@@ -51,15 +62,15 @@ cycle_deltas = table2array(cycle_deltas);
 % NOTE: The current approach of subsampling the colums for the ~isRef rows
 % excludes the CO2 check rows. I must remember to add these back in later.
 cycle_metadata = table();
-cycle_metadata.datetime = xp2018.TimeCode(~xp2018.IsRef__);
-cycle_metadata.filename = xp2018.FileHeader_Filename(~xp2018.IsRef__);
-cycle_metadata.sequenceRow = xp2018.Row(~xp2018.IsRef__);
-cycle_metadata.ASInlet = xp2018.AS_SIOInlet(~xp2018.IsRef__);
-cycle_metadata.ID1 = xp2018.Identifier1(~xp2018.IsRef__);
-cycle_metadata.method = xp2018.Method(~xp2018.IsRef__);
-cycle_metadata.scriptName = xp2018.ScriptName(~xp2018.IsRef__);
-cycle_metadata.gasConfig = xp2018.GasConfiguration(~xp2018.IsRef__);
-cycle_metadata.gasName = xp2018.GasName(~xp2018.IsRef__);
+cycle_metadata.datetime = importedData.TimeCode(~importedData.IsRef__);
+cycle_metadata.filename = importedData.FileHeader_Filename(~importedData.IsRef__);
+cycle_metadata.sequenceRow = importedData.Row(~importedData.IsRef__);
+cycle_metadata.ASInlet = importedData.AS_SIOInlet(~importedData.IsRef__);
+cycle_metadata.ID1 = importedData.Identifier1(~importedData.IsRef__);
+cycle_metadata.method = importedData.Method(~importedData.IsRef__);
+cycle_metadata.scriptName = importedData.ScriptName(~importedData.IsRef__);
+cycle_metadata.gasConfig = importedData.GasConfiguration(~importedData.IsRef__);
+cycle_metadata.gasName = importedData.GasName(~importedData.IsRef__);
 
 %% Reshape into Cycles-x-Isotope Ratio-x-Block
 % Identify the different blocks by the unique filenames
