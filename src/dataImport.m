62b3595116dba6ec9e852f55ac04533c460e8aab
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-04-01 13:46:16 -0700

    converts the code to store the metadata in structures of arrays

--- src/dataImport.m
+++ src/dataImport.m
@@ -9,7 +9,7 @@
 % disp({'Loading file 1: Working...'})
 % xp2018 = readtable('XP-2018(excelExportIntensityJDM).csv','Delimiter',',');
 % disp({'Loading file 1: Complete'}); disp({'Loading file 2: Working...'})
-%  xp2017 = readtable('XP-2017(excelExportIntensityJDM).csv','Delimiter',',');
+% xp2017 = readtable('XP-2017(excelExportIntensityJDM).csv','Delimiter',',');
 % disp({'Loading file 2: Complete'}); disp({'Loading file 3: Working...'})
 % xp2016 = readtable('XP-2016(excelExportIntensityJDM).csv','Delimiter',',');
 % disp({'Loading file 3: Complete'}); disp({'Loading file 4: Working...'})
@@ -27,6 +27,7 @@ importedData.TimeCode = datetime(datenum(importedData.TimeCode),'ConvertFrom','D
 importedData.Date = datestr(datenum(importedData.Date) + datenum('31 Dec 1999')); %Correct for two-character month '0018'
 importedData.IsRef__ = logical(importedData.IsRef__);
 importedData.Method = string(importedData.Method);
+importedData.Identifier1 = string(importedData.Identifier1);
 
 idxTimeCode = find(cellfun(@(varName) strcmp('TimeCode',varName),importedData.Properties.VariableNames)==1);
 importedData = sortrows(importedData,idxTimeCode);
@@ -82,7 +83,6 @@ cycle_deltas = table2array(cycle_deltas);
 % Also, the cycles all have exactly the same metadata so this step is
 % somewhat pointless.
 
-cycle_metadata = table();
 cycle_metadata.msDatetime = importedData.TimeCode(~importedData.IsRef__);
 cycle_metadata.filename = importedData.FileHeader_Filename(~importedData.IsRef__);
 cycle_metadata.sequenceRow = importedData.Row(~importedData.IsRef__);
@@ -93,13 +93,11 @@ cycle_metadata.scriptName = importedData.ScriptName(~importedData.IsRef__);
 cycle_metadata.gasConfig = importedData.GasConfiguration(~importedData.IsRef__);
 cycle_metadata.gasName = importedData.GasName(~importedData.IsRef__);
 
-metadata_cols = cycle_metadata.Properties.VariableNames;
-cycle_metadata = table2cell(cycle_metadata);
+metadata_fields = fieldnames(cycle_metadata)'; % Transpose it to a row vector so that it works as a loop index
 
 %% Reshape into Cycles-x-Isotope Ratio-x-Block
 % Identify the different blocks by the unique filenames
-iColName = string(metadata_cols)=='filename';
-[~,idx_blocks,~] = unique(cycle_metadata(:,iColName),'stable');
+[~,idx_blocks,~] = unique(cycle_metadata.filename,'stable');
 blockLengths = diff(idx_blocks); blockLengths(end+1)=length(cycle_deltas)-(idx_blocks(end)-1);
 
 % TAKE ONLY THE BLOCKS WITH 16 CYCLES! - This causes me to lose 156 blocks
@@ -112,34 +110,28 @@ longestBlock = max(blockLengths);
 
 % Fill the Array of Delta Values
 block_deltas = nan(longestBlock,size(cycle_deltas,2),numberOfBlocks);
-block_metadata = cell(longestBlock,size(cycle_metadata,2),numberOfBlocks);
 
 for ii = 1:length(idx_blocks)
     block_deltas(1:blockLengths(ii),:,ii) = cycle_deltas(idx_blocks(ii):idx_blocks(ii)+blockLengths(ii)-1,:);
-    block_metadata(1:blockLengths(ii),:,ii) = cycle_metadata(idx_blocks(ii):idx_blocks(ii)+blockLengths(ii)-1,:);
+    for jj = 1:numel(metadata_fields)
+        block_metadata.(metadata_fields{jj})(1:blockLengths(ii),ii) = cycle_metadata.(metadata_fields{jj})(idx_blocks(ii):idx_blocks(ii)+blockLengths(ii)-1,:);
+    end
 end
 
-% Keep One Line of Metadata Per Block, Rather than One Per Cycle
-% block_metadata = cycle_metadata(idx_blocks,:);
 
 %% Reshape into Cycles-x-Isotope Ratio-x-Block-x-Sample Aliquot
 % Define Methods that Are Run at the Start of Each New Sample
 sampleStartMethods = ["can_v_can"; "Automation_SA_Delay"; "Automation_SA"];
-iColName=string(metadata_cols)=='method';
 
-idx_working = false(size(block_metadata,3),1);
-method_names = vertcat(block_metadata{1,iColName,:});
+idx_working = false(size(block_metadata.msDatetime,2),1);
 
 for ii=1:length(sampleStartMethods)
-    idx_working = idx_working | method_names==sampleStartMethods(ii);
+    idx_working = idx_working | block_metadata.method(1,:)'==sampleStartMethods(ii);
 end
 
 % Find new aliquots by finding above methods or by finding the start of a
 % new sequence (sequence row decreases from N to 1).
-iColName = string(metadata_cols)=='sequenceRow';
-sequence_rows = vertcat(block_metadata{1,iColName,:});
-
-idx_SampleAliquots = find(idx_working | ([0; diff(sequence_rows)]<0));
+idx_SampleAliquots = find(idx_working | ([0; diff(block_metadata.sequenceRow(1,:)')]<0));
 aliquotLengths = diff(idx_SampleAliquots); aliquotLengths(end+1)=length(block_deltas)-(idx_SampleAliquots(end)-1);
 
 % TAKE ONLY THE ALIQUOTS WITH 4 OR 5 BLOCKS! - This causes me to lose 56
@@ -153,30 +145,28 @@ longestAliquot = max(aliquotLengths);
 
 % Reshape
 aliquot_deltas = nan(longestBlock,size(cycle_deltas,2),longestAliquot,numberOfAliquots);
-aliquot_metadata = cell(longestBlock,size(cycle_metadata,2),longestAliquot,numberOfAliquots);
+
 for ii = 1:length(idx_SampleAliquots)
     aliquot_deltas(:,:,1:aliquotLengths(ii),ii) = block_deltas(:,:,idx_SampleAliquots(ii):idx_SampleAliquots(ii)+aliquotLengths(ii)-1);
-    aliquot_metadata(:,:,1:aliquotLengths(ii),ii) = block_metadata(:,:,idx_SampleAliquots(ii):idx_SampleAliquots(ii)+aliquotLengths(ii)-1);
+    for jj=1:numel(metadata_fields)
+        aliquot_metadata.(metadata_fields{jj})(:,1:aliquotLengths(ii),ii) = block_metadata.(metadata_fields{jj})(:,idx_SampleAliquots(ii):idx_SampleAliquots(ii)+aliquotLengths(ii)-1);
+    end
 end
-% Could pull out metadata into a separate variable but it would have to be
-% a cell array. It can't be a table as aliquot_deltas is 4D so the metadata
-% would have to be 3D unless I just took one for each aliquot and excluded
-% information about the different blocks within the aliquot.
+
 
 %% Do Some Staistics on the Blocks
 % There are some weird looking blocks here that plot off the top of the
 % y-axis. I should take a closer look. Set a threshold for inclusion?
-iColName = string(metadata_cols)=='msDatetime';
 
 figure
 subplot(211)
-plot(vertcat(block_metadata{1,iColName,:}),blockLengths)
+plot(block_metadata.msDatetime(1,:),blockLengths)
 xlabel('Block Number'); ylabel('Number of Cycles in Block'); 
 title('Block Length (by method)');
 ylim([0 20])
 
 subplot(212)
-plot(vertcat(aliquot_metadata{1,iColName,1,:}),aliquotLengths)
+plot(squeeze(aliquot_metadata.msDatetime(1,1,:)),aliquotLengths)
 xlabel('Aliquot Number'); ylabel('Number of Blocks in Aliquot'); 
 title('Aliquot Lengths (by method)')
 ylim([0 10])
@@ -184,76 +174,79 @@ ylim([0 10])
 
 figure
 subplot(211)
-semilogy(vertcat(block_metadata{1,iColName,:}),squeeze(std(block_deltas)),'.');
+semilogy(block_metadata.msDatetime(1,:),squeeze(std(block_deltas)),'.');
 ylabel('Std Dev of Cycles in a Block [per mil]');
 ylim([0 150]);
 legend(delta_cols,'Location','N','Orientation','Horizontal');
 
 subplot(212)
-semilogy(vertcat(aliquot_metadata{1,iColName,1,:}),squeeze(nanstd(mean(aliquot_deltas,1),0,3)),'.');
+semilogy(squeeze(aliquot_metadata.msDatetime(1,1,:)),squeeze(nanstd(mean(aliquot_deltas,1),0,3)),'.');
 ylabel('Std Dev of Blocks in an Aliquot [per mil]')
 ylim([0 1500])
 
 
 %% PIS Correction
 block_means = nanmean(aliquot_deltas,1);
-iPIS = ~isnan(block_means(:,:,:,:));
 
-allDeltasPisCheck = sum(squeeze(iPIS),3); % sums the number of PIS blocks for each delta value
+iPIS = ~isnan(block_means(:,:,:,:)); % PIS aliquots are the aliquots with no nans for any of the blocks
+
+% First, check that there is a delta value for each measured ratio
+allDeltasPisCheck = sum(squeeze(iPIS),3); % sums the number of non-nan blocks for each delta value
 if length(unique(allDeltasPisCheck(:,5))) > 1 % if there is more than one unique value in the fifth column then some deltas are missing a PIS value
     warning('CAUTION: Some delta values are missing a PIS value')
 end
+
+ % Now restrict PIS blocks to just those with PIS as an identifier to weed
+ % out unusual aliquots where a fifth, non-PIS block was run
+iPIS = squeeze(iPIS(1,1,5,:)) & squeeze(aliquot_metadata.ID1(1,5,:))=='PIS';
 
 calcPis = nan(size(block_means,1),size(block_means,2)-3,1,size(block_means,4));
 calcPisRsq = nan(size(block_means,4),size(block_means,2)-3);
 calcPisImbal = nan(size(block_means,4),1);
 
-for ii=find(iPIS(1,1,5,:))' % find the indices of the PIS aliquots and loop through them, just look at the first cycle, delta value, and the fifth block for each aliquot
+for ii=find(iPIS)' % find the indices of the PIS aliquots and loop through them, just look at the first cycle, delta value, and the fifth block for each aliquot
     for jj=4:size(block_means,2) % loop all through delta values, skip the first three columns as these are voltages and pressure imbalance
 
         d = squeeze(block_means(:,jj,:,ii)); % response variable = the looped delta value from the looped aliquot
         G = [ones(size(d)) squeeze(block_means(:,3,:,ii))]; % predictor variable = the pressure imbalance (col 3) from the looped variable
+        m = (G'*G)\G'*d; % Calculate the PIS
         
-        m = (G'*G)\G'*d;
         r_sq = corrcoef(G(:,2),d).^2; % Find the r-squared correlation coefficient for the PIS test
-        [pImbal, idx] = max(abs(G(:,2)));
+        [pImbal, idx] = max(abs(G(:,2))); % Find the block with the max P Imbalance
         
         calcPis(1,jj-3,1,ii)=m(2);
         calcPisRsq(ii,jj-3) = r_sq(1,2);
-        calcPisImbal(ii) = pImbal * sign(G(idx,2)); % Find the pressure imbalance for the PIS block
+        calcPisImbal(ii) = pImbal * sign(G(idx,2));
     end
 end
 
-%%
-iColName = string(metadata_cols)=='ID1';
-id1 = squeeze(aliquot_metadata(1,iColName,5,:));
-
-iColName = string(metadata_cols)=='msDatetime';
-msDatetime = vertcat(aliquot_metadata{1,iColName,1,:});
-
+%% Plot a time-series of the PIS and related parameters
+% This is useful to identify aliquots where the PIS block did no run
+% correctly or where the r-squared is low, suggesting a poor determination
+% of the PIS. These cases can be filtered out below.
 
 stackedFig(3,'RelSize',[0.4 1.7 0.9],'Overlap',[-10 -10]);
 
 stackedFigAx(1)
-plot(vertcat(aliquot_metadata{1,iColName,1,:}),calcPisRsq,'s')
+plot(squeeze(aliquot_metadata.msDatetime(1,1,:)),calcPisRsq,'s')
 set(gca,'ColorOrderIndex',1)
-plot(msDatetime(~isnan(calcPisImbal)),calcPisRsq(~isnan(calcPisImbal),:),'-');
+plot(squeeze(aliquot_metadata.msDatetime(1,1,~isnan(calcPisImbal))),calcPisRsq(~isnan(calcPisImbal),:),'-');
 ylabel('r^2');
 ylim([0.7 1]);
 
 stackedFigAx(2)
-plot(vertcat(aliquot_metadata{1,iColName,1,:}),squeeze(calcPis),'o')
+plot(squeeze(aliquot_metadata.msDatetime(1,1,:)),squeeze(calcPis),'o')
 set(gca,'ColorOrderIndex',1)
-plot(msDatetime(~isnan(calcPisImbal)),squeeze(calcPis(:,:,:,~isnan(calcPisImbal))),'-')
+plot(squeeze(aliquot_metadata.msDatetime(1,1,~isnan(calcPisImbal))),squeeze(calcPis(:,:,:,~isnan(calcPisImbal))),'-')
 legend(delta_cols(4:end),'Orientation','Horizontal','Location','North')
 ylabel('PIS [per mil/per mil]');
 ylim([-0.03 0.03]);
 
 stackedFigAx(3)
-plot(vertcat(aliquot_metadata{1,iColName,1,:}),calcPisImbal,'^')
+plot(squeeze(aliquot_metadata.msDatetime(1,1,:)),calcPisImbal,'^')
 set(gca,'ColorOrderIndex',1)
-plot(msDatetime(~isnan(calcPisImbal)),calcPisImbal(~isnan(calcPisImbal)),'-','Color',lineCol(1));
-text(msDatetime,calcPisImbal,id1)
+plot(squeeze(aliquot_metadata.msDatetime(1,1,~isnan(calcPisImbal))),calcPisImbal(~isnan(calcPisImbal)),'-','Color',lineCol(1));
+text(squeeze(aliquot_metadata.msDatetime(1,1,:)),calcPisImbal,aliquot_metadata.ID1(1,5,:))
 ylabel('Pressure Imbalance [per mil]')
 ylim([-600 600])
 
