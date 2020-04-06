f05ee32eddba16e8be5d93cc543301362f1d8719
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-04-06 13:53:56 -0700

    identifies the spc aliquots and their replicates

--- src/spcPlotting.m
+++ src/spcPlotting.m
@@ -2,3 +2,22 @@
 % Identifies the data from the South Pole Ice Core and plots various parts
 % of the dataset.
 
+%% Identify the relevant aliquots and their replicates
+% Use only the aliquots that have 'SPICE' in the filename and than have a
+% decimal point (indicating a bottom depth) in the ID1 string.
+
+iSPC = contains(aliquot_metadata.filename(1,1,:),'SPICE') & contains(aliquot_metadata.ID1(1,1,:),'.');
+
+workingStr = sprintf('%s_*',aliquot_metadata.ID1(1,1,iSPC));
+workingStr(workingStr==' ') = '';
+workingStr = replace(workingStr,'a_*','a*');
+workingStr = replace(workingStr,'b_*','b*');
+workingStr = replace(workingStr,'c_*','c*');
+workingStr = replace(workingStr,'d_*','d*');
+
+bottomDepthRep = sscanf(workingStr,'%f%c*',[2 Inf]);
+
+rep = char(bottomDepthRep(2,:));
+iRepA = rep=='a'; iRepB = rep=='b'; iRepC = rep=='c'; iRepD = rep=='d';
+iNoRep = rep(~iRepA & ~iRepB & ~iRepC & ~iRepD);
+
