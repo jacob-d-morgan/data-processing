90e30d8455c95fc7da9c5f010ea528ccfc67a333
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-15 11:28:39 -0700

    adds description pisRejectionMethodTesting.m

--- src/pisRejectionMethodTesting.m
+++ src/pisRejectionMethodTesting.m
@@ -1,3 +1,8 @@
 %% PIS Outlier Rejection Method Testing
 % Tests and plots several different methods for rejecting 'bad' PIS values.
+% I always reject any PIS experiment where the P Imbalance is less than
+% 100 mV but I need a more specific criteria to reject 'bad' PIS values for
+% each delta value.
+
+
 
