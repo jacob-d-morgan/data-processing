0941ee44d7ebbd6e61b1686c1b5006d977738f78
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-15 14:33:32 -0700

    adds the movmed plus or minus three MAD (of whole time-series) method

--- src/pisRejectionMethodTesting.m
+++ src/pisRejectionMethodTesting.m
@@ -58,6 +58,51 @@ for ii =1:length(movWindow)
     xlim(datenum(["01-Jan-2016" "01-Jan-2019"]));
     datetick('x','dd-mmm','KeepLimits')
     title(['Window = ' num2str(movWindow(ii)) ' days'])
+    stackedFigReset;
+
+end
+
+figure
+plot(movWindow,numRej)
+xlabel('Window Size [days]');
+ylabel('Number of Rejections')
+legend(delta_cols)
+
+
+%% Moving Median +/- 3 Constant MedAD
+% Reject PIS values that are more than three median absolute deviations
+% away from a moving median of the time series. Test various values for the
+% width of the moving median window.
+
+movWindow = 7:7:7*4*4;
+numRej = nan(length(movWindow),numel(delta_cols));
+for ii =1:length(movWindow)
+    stackedFig(numel(delta_cols));
+    for jj = 1:numel(delta_cols)
+        stackedFigAx(jj)
+        x = aliquot_metadata.msDatenum(~isnan(calcPisImbal),1,1);
+        y = calcPis(~isnan(calcPisImbal),jj,1,1);
+        
+        cen = movmedian(y,movWindow(ii),'omitnan','SamplePoints',x);
+        MedAD = mad(y,1); % flag = 1 calculates Median AD c.f. Mean AD
+        low = cen - 3*MedAD*-1/(sqrt(2)*erfcinv(3/2)); % scaling factor used by isoutlier() makes MAD ~ StdDev for normally dist data.
+        upp = cen + 3*MedAD*-1/(sqrt(2)*erfcinv(3/2));
+        iRej = (y > upp) | (y < low);
+        
+        H=shadedErrorBar(x,cen,[upp-cen cen-low],'-k'); delete(H.edge); H.patch.FaceColor = lineCol(9)*1.3; H.mainLine.LineWidth = 1;
+        plot(x(~iRej),y(~iRej),'o','Color','none','MarkerFaceColor',lineCol(jj))
+        errorbar(x(iRej),y(iRej),[],[],repmat(movWindow(ii)/2,sum(iRej),1),repmat(movWindow(ii)/2,sum(iRej),1),'xk','LineWidth',2)
+        ylabel(delta_cols{jj})
+        
+        numRej(ii,jj) = sum(iRej(:,:,1,1));
+    end
+    
+    stackedFigAx;
+    xlim(datenum(["01-Jan-2016" "01-Jan-2019"]));
+    datetick('x','dd-mmm','KeepLimits')
+    title(['Window = ' num2str(movWindow(ii)) ' days'])
+    stackedFigReset;
+    maximize;
 
 end
 
