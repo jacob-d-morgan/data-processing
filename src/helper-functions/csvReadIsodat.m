c11bc2a859759f73c7dd6c04e0842abec7611920
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-18 11:01:20 -0700

    Initial commit for csvReadIsodat.m

---
+++ src/helper-functions/csvReadIsodat.m
@@ -0,0 +1,8 @@
+function importedData = csvReadIsodat(filename)
+% CSVREADISODAT   Import an .csv file created by reprocessing ISODAT files
+%   IMPORTEDDATA = CSVREADISODAT(FILENAME) reads the file(s) in FILENAME
+%   and outputs them in the table, IMPORTEDDATA. Files are read using
+%   READTABLE, the built-in MATLAB function.
+
+
+end
