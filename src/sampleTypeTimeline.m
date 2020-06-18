8c999d97eb5f9820ddb5ba63f017609a46392452
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-17 17:05:49 -0700

    Initial commit for sampleTypeTimeline.m

---
+++ src/sampleTypeTimeline.m
@@ -0,0 +1,7 @@
+%% Sample Type Timeline
+% Plots a 'timeline' of the different categories of the imported data. This
+% allows me to identify the different batches of CS, LJA, and which ice
+% cores they are applied to.
+% It also allows me to manually plot the date of significant changes to the
+% MS, such as a refocussing, or significant adjustments to the method, such
+% as the implementation of the O2-consumption correction.
