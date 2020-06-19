a4b37b2d7f0f7bd4d300a712aa13ad8d7443b861
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-19 10:14:49 -0700

    Initial commit for calcCycles.m

---
+++ src/helper-functions/calcCycles.m
@@ -0,0 +1,10 @@
+function [cycle_deltas,cycle_metadata] = calcCycles(importedData,isRef)
+% CALCCYCLES converts imported ISODAT data to cycle deltas and metadata
+%   [CYCLEDELTAS,CYCLEMETADATA] = CALCCYCLES(IMPORTEDDATA,ISREF) calculates
+%   delta values from the imported data input, using the logical ISREF
+%   variable as the indicator of sample (false) and reference (true) 
+%   measurements. CYCLEMETADATA is the metadata from the sample rows.
+
+
+
+end
