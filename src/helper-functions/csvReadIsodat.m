fda764488578fc8ead7534b2887aac6e23affb53
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-18 17:44:08 -0700

    first attempt at csvReadIsodat.m

--- src/helper-functions/csvReadIsodat.m
+++ src/helper-functions/csvReadIsodat.m
@@ -5,4 +5,79 @@ function importedData = csvReadIsodat(filename)
 %   READTABLE, the built-in MATLAB function.
 
 
+% -------------------------------------------------------------------------
+
+% Check Input
+if isstring(filename)
+    filesToRead = filename;
+elseif ischar(filename)
+    filesToRead = string(filename);
+elseif iscellstr(filename)
+    filesToRead = string(filename);
+else
+    error('INVALID INPUT: Input variable must be a string array, char array, or cell array of char arrays');
+end
+
+
+% Check Import Options for Each File
+opts = cell(1,length(filesToRead));
+numVars = nan(1,length(filesToRead));
+varNames = cell(1,length(filesToRead));
+varTypes = cell(1,length(filesToRead));
+
+for ii = 1:length(filesToRead)
+    opts{ii} = detectImportOptions(filesToRead(ii),'ReadVariableNames',true,'Delimiter',',');
+    numVars(ii) = length(opts{ii}.VariableNames);
+    varNames{ii} = string(opts{ii}.VariableNames);
+    varTypes{ii} = string(opts{ii}.VariableTypes);
+end
+
+
+if length(unique(numVars)) > 1
+    error(join(['Invalid Input: Input files appear to have a different number of variables. ', ...
+        'detectImportOpts suggests that the input files have ', ...
+        join(string(numVars(1:end-1)),', ') ', and ' string(numVars(end)) ' variables repectively.' ],''));
+end
+
+if numVars(1) ~= 38
+    warning(['Expected the inuput files to contain 38 variables but the files contains ' num2str(numVars(1)), ...
+        '. Has there been a change in the reprocessing template?']);
+end
+
+if ~isequal(varNames{:})
+    error('Invalid Input: Input files appear to have different vairable names or a different order of variables.')
+end
+
+if ~isequal(varTypes{:})
+    error('Invalid Input: Input files appear to have different vairable types or a different order of variable types.')
+end
+
+
+% Set My Default Import Options (Based off Auto-detected Options)
+optsToUse = opts{1};
+optsToUse = setvartype(optsToUse,'IsRef__','logical');
+optsToUse = setvartype(optsToUse,'PeakCenter','logical');
+optsToUse = setvartype(optsToUse,'Pressadjust','logical');
+optsToUse = setvartype(optsToUse,'Background','logical');
+optsToUse = setvartype(optsToUse,contains(optsToUse.VariableNames,'rIntensity'),'double'); % Import all the intensities as doubles, even if they are all NaN
+optsToUse = setvartype(optsToUse,string(optsToUse.VariableTypes)=='char','string'); % Import all text as string arrays
+optsToUse = setvartype(optsToUse,'TimeCode','datetime');
+optsToUse = setvaropts(optsToUse,'TimeCode','InputFormat','yyyy/MM/dd HH:mm:ss','DatetimeFormat','yyyy-MM-dd HH:mm:ss');
+optsToUse = setvartype(optsToUse,'Date','datetime');
+optsToUse = setvaropts(optsToUse,'Date','InputFormat','MM/dd/yy','DatetimeFormat','yyyy-MM-dd HH:mm:ss');
+optsToUse.VariableNames(string(optsToUse.VariableNames)=='Row')={'SequenceRow'}; % Otherwise this inteferes with table.Row, which is already a built-in MATLAB command
+
+
+% Read In Files
+importedData = table;
+for ii=1:length(filesToRead)
+    disp(compose('Reading File %i: Working...',ii))
+    readFile = readtable(filesToRead(ii),optsToUse);
+    importedData = [importedData; readFile];
+    disp(compose('Reading File %i: Complete',ii))
+end
+
+importedData = sortrows(importedData,'TimeCode');
+
+
 end
