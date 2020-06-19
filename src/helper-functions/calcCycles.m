7d8459bc2057f2d2addb60e916302749710936ef
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-19 10:34:03 -0700

    Add to the description of calcCycles.m

--- src/helper-functions/calcCycles.m
+++ src/helper-functions/calcCycles.m
@@ -1,9 +1,14 @@
 function [cycle_deltas,cycle_metadata] = calcCycles(importedData,isRef)
 % CALCCYCLES converts imported ISODAT data to cycle deltas and metadata
 %   [CYCLEDELTAS,CYCLEMETADATA] = CALCCYCLES(IMPORTEDDATA,ISREF) calculates
-%   delta values from the imported data input, using the logical ISREF
-%   variable as the indicator of sample (false) and reference (true) 
-%   measurements. CYCLEMETADATA is the metadata from the sample rows.
+%   delta values from the table of imported data. 
+%   ISREF is the variable that indicates whether a given line is a sample
+%   (false) or reference (true) measurement. It can be either (1) a logical
+%   variable with one column and the same number of rows as importedData,
+%   (2) the name of a variable in importedData, (3) the index of a variable
+%   in importedData, or (4) a logical index with one row and the same
+%   number of columns as imported data.
+%   CYCLEMETADATA is the metadata from the sample rows.
 
 
 
