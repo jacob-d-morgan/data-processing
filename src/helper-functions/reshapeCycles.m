48a1d57557158b0b89c72aaf02e62f372652cf62
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-06-19 18:26:08 -0700

    Initial commit to reshapeCycles.m

---
+++ src/helper-functions/reshapeCycles.m
@@ -0,0 +1,5 @@
+function aliquot_deltas = reshapeCycles(cycle_deltas)
+% RESHAPECYCLES converts cycle deltas to an aliquot-delta-block-cycle array
+%   ALIQUOTDELTAS = RESHAPECYCLES(CYCLEDELTAS) reshapes CYCLEDELTAS to the
+%   array ALIQUOTDELTAS, which contains the elements of CYCLEDELTAS in a
+%   aliquot-by-delta-by-block-by-cycle structure.
