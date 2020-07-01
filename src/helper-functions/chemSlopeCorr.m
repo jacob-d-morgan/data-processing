615bb5cbc9af2701be50658835e5e922d59037f4
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-07-01 15:23:18 -0700

    Initial commit for chemSlopeCorr.m

---
+++ src/helper-functions/chemSlopeCorr.m
@@ -0,0 +1,10 @@
+function [aliquot_deltas_csCorr] = chemSlopeCorr(aliquot_deltas,aliquot_metadata)
+% CSCORR corrects all the delta values in ALIQUOT_DELTAS for the effect of
+% the chemical slope. The correction is determined using the delta values
+% from the CS experiment aliquots and their metadata in ALIQUOT_DELTA and
+% ALIQUOT_METADATA.
+%
+%
+%  ------------------------------------------------------------------------
+
+end
