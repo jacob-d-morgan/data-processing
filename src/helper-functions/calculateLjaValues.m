00c80d10be1c89245b63d826f120972521f20731
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-07-08 17:50:39 -0700

    Initial commit for calculateLjaValues.m

---
+++ src/helper-functions/calculateLjaValues.m
@@ -0,0 +1,9 @@
+function [calcLja stats] = calculateLjaValues(aliquot_deltas,aliquot_metadata,iLjaToUse)
+% CALCULATELJAVALUES calculates the mean of each set of LJA samples run in
+% order to calibrate a standard can to the ultimate atmospheric standard.
+%
+% -------------------------------------------------------------------------
+
+
+
+
