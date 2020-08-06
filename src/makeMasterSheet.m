f14985eba066f33cbf334cf02b5d5883fed31c23
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-08-06 12:08:09 -0700

    Initial commit for makeMasterSheet.m

---
+++ src/makeMasterSheet.m
@@ -0,0 +1,19 @@
+function masterSheet = makeMasterSheet(filesToImport)
+% MAKEMASTERSHEET Imports and compiles ISODAT reprocessed .csv files into a
+% working dataset organised by analysis date.
+%   MASTERSHEET = MAKEMASTERSHEET(FILESTOIMPORT) generates a "Master Sheet"
+%   from the data in the ISODAT-reprocessed .csv files in FILESTOIMPORT.
+%
+%   The master sheet is a compilation of the relevant aliquots from the
+%   input files, including data from Standard Can, PIS, Chem Slope, and La
+%   Jolla Air analyses, as well as any true ice core or firn air samples.
+%   The aliquots are arranged by analysis date.
+%
+% -------------------------------------------------------------------------
+
+
+
+
+
+
+end
