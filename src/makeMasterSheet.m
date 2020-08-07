87ec2cb8e4018e5db7e54d3ffbb5d9766b9a64d8
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2020-08-07 11:57:11 -0700

    Add a function to arrange data into one table, aka the 'master sheet'
    The function compiles the metadata, raw delta values, corrected delta
    values, and the correction parameters into individual tables and then
    compiled all of these into one master table.

--- src/makeMasterSheet.m
+++ src/makeMasterSheet.m
@@ -11,9 +11,140 @@ function masterSheet = makeMasterSheet(filesToImport)
 %
 % -------------------------------------------------------------------------
 
+%% Parse Inputs
+
+narginchk(1,inf)
 
 
+%% Import Raw Data
+% Imports the 'standard' raw dataset of aliquot delta values and their
+% metadata. Does not include any cycles with a gas configuration different
+% to Air+ or where the 28N2 beam is either saturated or absent, or cycles
+% with a non-standard (different than the mode) number of cycles or blocks.
+
+[aliquot_deltas,metadata,aliquot_deltas_pis,aliquot_metadata_pis] = makeRawDataset(filesToImport,'includePIS',true);
+
+aliquot_metadata = metadata.metadata;
+delta_names = metadata.delta_names;
+delta_labels = metadata.delta_labels;
+delta_units = metadata.delta_units;
+
+metadata_fields = string(fieldnames(aliquot_metadata))';
 
 
+%% Make PIS Correction
+% Correct the delta values in aliquot_deltas for the effect of imbalance in
+% the total pressure of gas in the source.
+
+% Calculate PIS Values
+[calcPis,pisStats] = calculatePisValues(aliquot_deltas,aliquot_metadata,aliquot_deltas_pis,aliquot_metadata_pis);
+
+% Make PIS Correction
+calcPis(pisStats.rejections) = nan;
+[aliquot_deltas_pisCorr,PIS] = makePisCorr(aliquot_deltas,aliquot_metadata.msDatetime,aliquot_metadata.pressureImbal,calcPis);
+
+%% Make Chemical Slope Correction
+% Correct the delta values in aliquot_deltas_pisCorr for the effect of
+% different gas ratios in the source.
+
+% Identify the CS Experiment Aliquots
+iCS = contains(aliquot_metadata.ID1(:,1,1),'CS');
+iCS_AddO2 = iCS & contains(aliquot_metadata.ID1(:,1,1),{'15','N'});
+iCS_AddN2 = iCS & contains(aliquot_metadata.ID1(:,1,1),{'18','O'});
+
+% == MANUALLY INCLUDE THE ONLY 2016-02-09 REP-0 IN BOTH CS EXPERIMENTS == %
+iCS_AddN2(aliquot_metadata.msDatetime(:,1,1)=={'2016-02-08 13:27:59'}) = true;
+% ======================================================================= %
+
+% Calculate the Univariate (N2 & O2 Isotopes, Ar/N2 Ratio) Chem Slopes
+[calcCS_15N,csStats_15N] = calculateChemSlope(aliquot_deltas_pisCorr(:,6,:,:),aliquot_deltas_pisCorr(:,1,:,:),aliquot_metadata,iCS_AddO2);
+[calcCS_ArN2,csStats_ArN2] = calculateChemSlope(aliquot_deltas_pisCorr(:,6,:,:),aliquot_deltas_pisCorr(:,7,:,:),aliquot_metadata,iCS_AddO2);
+[calcCS_18O,csStats_18O] = calculateChemSlope((1/(aliquot_deltas_pisCorr(:,6,:,:)/1000+1)-1)*1000,aliquot_deltas_pisCorr(:,2,:,:),aliquot_metadata,iCS_AddN2);
+[calcCS_17O,csStats_17O] = calculateChemSlope((1/(aliquot_deltas_pisCorr(:,6,:,:)/1000+1)-1)*1000,aliquot_deltas_pisCorr(:,3,:,:),aliquot_metadata,iCS_AddN2);
+
+% Calculate the Bivariate (Ar Isotopes) Chem Slopes
+x_temp = [((aliquot_deltas_pisCorr(:,7,:,:)./1000+1).^-1-1)*1000 ((aliquot_deltas_pisCorr(:,6,:,:)/1000+1)./(aliquot_deltas_pisCorr(:,7,:,:)./1000+1)-1)*1000]; % predictor variables = dN2/Ar AND dO2Ar (= [q_o2n2/q_arn2 -1]*1000)
+[calcCS_4036Ar,csStats_36Ar] = calculateChemSlope(x_temp,aliquot_deltas_pisCorr(:,4,:,:),aliquot_metadata,iCS_AddN2 | iCS_AddO2,true);
+[calcCS_4038Ar,csStats_38Ar] = calculateChemSlope(x_temp,aliquot_deltas_pisCorr(:,5,:,:),aliquot_metadata,iCS_AddN2 | iCS_AddO2,true);
+
+% Make CS Corrections
+csValues = [{calcCS_15N} {calcCS_18O} {calcCS_17O} {calcCS_ArN2} {calcCS_4036Ar} {calcCS_4038Ar}];
+csRegressors = [
+    {aliquot_deltas_pisCorr(:,delta_names=='d15N',:,:);}, ...
+    {aliquot_deltas_pisCorr(:,delta_names=='d18O',:,:)}, ...
+    {aliquot_deltas_pisCorr(:,delta_names=='d17O',:,:)}, ...
+    {aliquot_deltas_pisCorr(:,delta_names=='dArN2',:,:);}, ...
+    {aliquot_deltas_pisCorr(:,delta_names=='d4036Ar',:,:)}, ...
+    {aliquot_deltas_pisCorr(:,delta_names=='d4038Ar',:,:);}, ...
+    ];
+csPredictors = [
+    {aliquot_deltas_pisCorr(:,delta_names=='dO2N2',:,:);}, ...
+    {((aliquot_deltas_pisCorr(:,delta_names=='dO2N2',:,:)/1000+1).^-1-1)*1000}, ...
+    {((aliquot_deltas_pisCorr(:,delta_names=='dO2N2',:,:)/1000+1).^-1-1)*1000}, ...
+    {aliquot_deltas_pisCorr(:,delta_names=='dO2N2',:,:);}, ...
+    {[((aliquot_deltas_pisCorr(:,delta_names=='dArN2',:,:)/1000+1).^-1-1)*1000 ((aliquot_deltas_pisCorr(:,delta_names=='dO2N2',:,:)/1000+1)./(aliquot_deltas_pisCorr(:,delta_names=='dArN2',:,:)/1000+1)-1)*1000]}, ...
+    {[((aliquot_deltas_pisCorr(:,delta_names=='dArN2',:,:)/1000+1).^-1-1)*1000 ((aliquot_deltas_pisCorr(:,delta_names=='dO2N2',:,:)/1000+1)./(aliquot_deltas_pisCorr(:,delta_names=='dArN2',:,:)/1000+1)-1)*1000]}, ...
+    ];
+
+csCorr = cell(size(csValues));
+CS = cell(size(csValues));
+for ii = 1:length(csValues)
+    [csCorr{ii},CS{ii}] = makeCsCorr(csRegressors{ii},aliquot_metadata.msDatetime,csPredictors{ii},csValues{ii});
+end
+
+aliquot_deltas_pisCorr_csCorr = aliquot_deltas_pisCorr;
+aliquot_deltas_pisCorr_csCorr(:,[1 2 3 7 4 5],:,:) = [csCorr{:}];
+
+
+%% Make the LJA Correction
+% Correct the delta values in aliquot_deltas_pisCorr_csCorr so that they
+% are measured relative to La Jolla Air.
+%
+% Identifies analyses of LJA made during identical MS conditions (i.e. same
+% filament, std can etc.) and calculates the mean of these aliquots, after
+% rejecting outliers. The mean of all the aliquots is used to normalize all
+% aliquots measured under identical MS conditions, unless there is a trend
+% in the LJA values. In this case, the extrapolated values are used.
+
+% Calculate LJA Values
+iLja = contains(aliquot_metadata.ID1(:,1,1),'LJA');
+[ljaValues,ljaStats] = calculateLjaValues(aliquot_deltas,aliquot_metadata,iLja);
+
+% Make LJA Correction
+[aliquot_deltas_pisCorr_csCorr_ljaCorr,LJA] = makeLjaCorr(aliquot_deltas_pisCorr_csCorr,aliquot_metadata.msDatetime(:,1,1),ljaStats,ljaValues);
+
+
+%% Assemble Into Final Output
+% Unite all of the useful data and metadata into tables and then output all
+% the tables as one master table.
+
+% Make Table of Metadata
+metadata = struct2table(aliquot_metadata);
+
+% Make Table of Raw Delta Values
+deltas_raw = table();
+for ii = 1:numel(delta_names)
+    deltas_raw = [deltas_raw table(aliquot_deltas(:,ii,:,:),'VariableNames',cellstr(delta_names(ii)))];
+end
+
+% Make Table fo Fully Corrected Delta Values
+deltas_corr = table();
+for ii = 1:numel(delta_names)
+    deltas_corr = [deltas_corr table(aliquot_deltas_pisCorr_csCorr_ljaCorr(:,ii,:,:),'VariableNames',cellstr(delta_names(ii)))];
+end
+
+% Make Tables of PIS, CS, and LJA Values Used
+cs_names = delta_names([1:3 7 4:5]);
+csLog = table;
+for ii = 1:numel(cs_names)
+    csLog = [csLog table(CS{ii},'VariableNames',cellstr(cs_names(ii)))];
+end
+
+pisLog = array2table(PIS,'VariableNames',cellstr(delta_names));
+ljaLog = array2table(LJA,'VariableNames',cellstr(delta_names));
+corrections = table(pisLog,csLog,ljaLog,'VariableNames',{'PIS','CS','LJA'});
+    
+
+masterSheet = table(metadata,deltas_raw,deltas_corr,corrections,'VariableNames',{'metadata','deltas_raw','deltas_corr','corrections'});
 
 end
