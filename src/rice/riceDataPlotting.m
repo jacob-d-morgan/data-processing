8eeea5f4b0cc616117e0ddb2f301b515ecc745eb
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2021-01-22 16:23:37 -0800

    Extract RICE bottom depths and generate dataset

--- src/rice/riceDataPlotting.m
+++ src/rice/riceDataPlotting.m
@@ -4,3 +4,35 @@
 
 clearvars; cluk;
 clc;
+
+%% Import Data
+% Load the relevant csv files and compile the data into the RICE master
+% sheet and ice core dataset.
+
+% Generate Master Sheet
+filesToImport = [
+    "XP-2014(exportJDM).csv";...
+    "XP-2015(exportJDM).csv";...
+    ];
+
+riceMasterSheet = makeMasterSheet(filesToImport);
+
+%%
+% Generate Ice Core Dataset
+iceCoreID = 'RICE';
+exp2match = '^\s?(?<depth>\d{2,3}([\.|_]\d{1,2})?)(?<rep>\s\w)?-\d{4,4}$';
+tokens = regexp(riceMasterSheet.metadata.fileNameIsodat(:,1),exp2match,'tokens');
+
+exp2match = '^b-\d{4,4}$';
+tokensRepB = regexp(riceMasterSheet.metadata.fileNameIsodat(:,1),exp2match);
+
+bD = repmat("",size(tokens));
+bD(~cellfun(@isempty,tokens)) = cellfun(@(x) x{1}(1),tokens(~cellfun(@isempty,tokens)));
+bD(~cellfun(@isempty,tokensRepB)) = bD(find(~cellfun(@isempty,tokensRepB))-1);
+
+iRICE = contains(riceMasterSheet.metadata.fileRelPath(:,1,1),iceCoreID) & bD~="";
+
+bottomDepth = str2double(replace(bD,"_","."));
+bottomDepth = bottomDepth(~isnan(bottomDepth));
+
+rice = makeIceCoreDataset(riceMasterSheet,iRICE,bottomDepth);
