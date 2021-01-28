b7a7f25b4d60c6cf961ece3a645901e0377d7084
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2021-01-28 11:24:45 -0800

    Calculate top depths and generate NEEM dataset
    Extract the bag numbers from the NEEM metadata and use it to calculate
    the top depth for the samples. This one is a little tricky as there are
    seemingly several different numbering conventions for the bags:
     - For most samples, the bag numbers start at 1 at 0 m depth and
     increase by increments of 0.55 m.
     - For 'Transition' or 'T' samples, the bags were renumbered
     starting at 2580 (1418.45 m, see NEEM_d15N_Transition_v1 1.xls). The
     top depths still increase by 0.55 m for each bag but samples were cut
     from multiple different depth ranges within a bag.
     - For 'X' samples, it is not yet clear what their bottom depth should
     be. Ignoring the X does not look right after comparing data between
     the X samples and regular samples.
    More to follow once I resolve this with Jeff/Ross.

--- src/neem/neemDataPlotting.m
+++ src/neem/neemDataPlotting.m
@@ -16,3 +16,56 @@ filesToImport = [
     ];
 
 neemMasterSheet = makeMasterSheet(filesToImport);
+
+%% Generate Ice Core Dataset
+% Identify the ice samples within the relevant folder and extract the
+% bottom depth from the metadata to use as an input to makeIceCoreDataset.
+
+% Extract Bottom Depths from Metadata
+exp2match = '^(NEEM\s)?(?<bagNum>(X\s*)?\d{3,4})\s?(?<rep>\w)?';
+tokens = regexpi(neemMasterSheet.metadata.fileNameUser(:,1,1),exp2match,'tokens');
+% This expression captures all regular samples. The first token ('NEEM\s') 
+% is empty for all but 4 samples and the Bag Number is the second token.
+% The expression also captures a few BYRD samples, which are filtered out
+% below using the fileRelPath.
+
+exp2match = '(?<bagNum>T\s\d{1,4})\s(?<bagDepths>\d{1,2}-\d{1,2})';
+tokensT = regexp(neemMasterSheet.metadata.fileNameUser(:,1,1),exp2match,'tokens');
+% This expression captures the samples from the Bolling Transition, which
+% have a different bag number convention and also include the depths of the
+% sample within the bag separated by a hyphen.
+
+
+% Identify NEEM Ice Samples
+iRegular = contains(neemMasterSheet.metadata.fileRelPath(:,1,1),'NEEM') & ...
+        ~cellfun(@isempty,tokens);
+iTransition = contains(neemMasterSheet.metadata.fileRelPath(:,1,1),'NEEM') & ...
+               ~cellfun(@isempty,tokensT); 
+iNEEM = iRegular | iTransition;
+
+
+% Combine Bag Numbers and Depths
+bagNum = repmat("",size(tokens));
+bagNum(iRegular) = cellfun(@(x) x{1}(2),tokens(iRegular));
+bagNum(iTransition) = cellfun(@(x) x{1}(1),tokensT(iTransition));
+bagNum = str2double(erase(bagNum,{'x','X','T',' '}));
+
+bagDepthRange = repmat("",size(tokensT));
+bagDepthRange(iTransition) = cellfun(@(x) x{1}(2),tokensT(iTransition));
+bagDepths = str2double(split(bagDepthRange(bagDepthRange~=""),'-'));
+
+
+% Assign Depths
+% N.B. The Danes report Top Depths for NEEM that are just the sum of the
+% number of 55 cm bags that have come before. The SIO piece seems to always
+% comprise the top 5 or 10 cm of each bag (unless noted, e.g. for the 
+% transition pieces). The bags for the Transition samples were renumbered
+% starting from bag 2580, which has a top depth of 1418.85 m.
+topDepth = nan(size(bagNum));
+topDepth(iRegular) = (bagNum(iRegular)-1)*0.55;
+topDepth(iTransition) = 1418.45 + (bagNum(iTransition)-1)*0.55 + bagDepths(:,1)/100;
+topDepth = topDepth(iNEEM);
+
+
+% Generate Ice Core Dataset
+neem = makeIceCoreDataset(neemMasterSheet,iNEEM,topDepth);
