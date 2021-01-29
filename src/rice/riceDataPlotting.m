0821c385a519ac37884946a9f8ebf965fce3cabd
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2021-01-29 10:33:16 -0800

    Adjust RICE regexp to catch missing samples. Add comments.

--- src/rice/riceDataPlotting.m
+++ src/rice/riceDataPlotting.m
@@ -20,9 +20,18 @@ riceMasterSheet = makeMasterSheet(filesToImport);
 %% Generate Ice Core Dataset
 % Identify the ice samples within the relevant folder and extract the
 % bottom depth from the metadata to use as an input to makeIceCoreDataset.
+% For RICE, I choose to use the ISODAT File Name because the User File Name
+% is trimmed incorrectly by the code that makes a guess at the User File
+% Name by removing line numbers (i.e. the d{1,3}_* pattern). 
+% I could use ID1 instead but there are a few cases where the ID1 seems to
+% not have been updated by the user (see 2015-03-26). Presumably the
+% samples were run manually, rather than with the automation, so the user
+% manually entered a new file name but forgot to clear the ID1 from the
+% previous sample. There is one other sample (2015-02-03 21:38) that is
+% missing an ID1, presumable also because it was run manually.
 
 % Extract Bottom Depths from Metadata
-exp2match = '^\s?(?<depth>\d{2,3}([\.|_]\d{1,2})?)(?<rep>\s\w)?-\d{4,4}$';
+exp2match = '^\s?(?<depth>\d{2,3}([\.|_]\d{1,2})?)(?<rep>\s\w)?_?-\d{4,4}$';
 tokens = regexp(riceMasterSheet.metadata.fileNameIsodat(:,1),exp2match,'tokens');
 % This expression matches all of the 'Rep A' samples and also some of the
 % 'Rep B' samples that are labelled with bottom depths. Many of the 'Rep B'
@@ -46,4 +55,8 @@ bD_temp(~cellfun(@isempty,tokensRepB)) = bD_temp(find(~cellfun(@isempty,tokensRe
 bottomDepth = str2double(replace(bD_temp,"_","."));
 
 % Generate Ice Core Dataset
-rice = makeIceCoreDataset(riceMasterSheet,iRICE,bottomDepth);
+rice = makeIceCoreDataset(riceMasterSheet,iRICE & ~isnan(bottomDepth),bottomDepth(iRICE & ~isnan(bottomDepth)));
+% Including the isnan clause filters out one problematic NaN value that
+% arises from assigning a NaN depth to a Rep B that has no corresponding
+% Rep A (so the bottom depth is NaN). The Rep A is NaN because it has two
+% decimal places/underscores so is not captured by the regexp.
