3a03990ee9929d4e6e7b0967132fd2bf34cea169
jacob-d-morgan <jacobmorgan@hotmail.co.uk>
2021-01-29 12:22:56 -0800

    Update NEEM script to include the 'X samples'.
    I was able to locate the depths that the 'x samples' correspond to
    for NEEM. This commit calculates their top depths and includes a few
    comments about these samples.

--- src/neem/neemDataPlotting.m
+++ src/neem/neemDataPlotting.m
@@ -22,12 +22,17 @@ neemMasterSheet = makeMasterSheet(filesToImport);
 % bottom depth from the metadata to use as an input to makeIceCoreDataset.
 
 % Extract Bottom Depths from Metadata
-exp2match = '^(NEEM\s)?(?<bagNum>(X\s*)?\d{3,4})\s?(?<rep>\w)?';
+exp2match = '^(?<bagNum>\d{3,4})\s?(?<rep>\w)?';
 tokens = regexpi(neemMasterSheet.metadata.fileNameUser(:,1,1),exp2match,'tokens');
 % This expression captures all regular samples. The first token ('NEEM\s') 
 % is empty for all but 4 samples and the Bag Number is the second token.
 % The expression also captures a few BYRD samples, which are filtered out
 % below using the fileRelPath.
+
+exp2match = '^(?:NEEM\s)?(?<bagNum>X\s*\d{3,4})\s?(?<rep>\w)?';
+tokensX = regexpi(neemMasterSheet.metadata.fileNameUser(:,1,1),exp2match,'tokens');
+% This expression captures the samples from bags labelled with an X. It is,
+% as yet, unclear what depths these samples correspond to.
 
 exp2match = '(?<bagNum>T\s\d{1,4})(\s(?<bagDepths>\d{1,2}-\d{1,2}))?';
 tokensT = regexp(neemMasterSheet.metadata.fileNameUser(:,1,1),exp2match,'tokens');
@@ -38,15 +43,18 @@ tokensT = regexp(neemMasterSheet.metadata.fileNameUser(:,1,1),exp2match,'tokens'
 
 % Identify NEEM Ice Samples
 iRegular = contains(neemMasterSheet.metadata.fileRelPath(:,1,1),'NEEM') & ...
-        ~cellfun(@isempty,tokens);
+           ~cellfun(@isempty,tokens);
+iX = contains(neemMasterSheet.metadata.fileRelPath(:,1,1),'NEEM') & ...
+     ~cellfun(@isempty,tokensX);
 iTransition = contains(neemMasterSheet.metadata.fileRelPath(:,1,1),'NEEM') & ...
-               ~cellfun(@isempty,tokensT); 
-iNEEM = iRegular | iTransition;
+              ~cellfun(@isempty,tokensT); 
+iNEEM = iRegular | iX | iTransition;
 
 
-% Combine Bag Numbers and Depths
+% Combine Bag Numbers and Depth Ranges
 bagNum = repmat("",size(tokens));
-bagNum(iRegular) = cellfun(@(x) x{1}(2),tokens(iRegular));
+bagNum(iRegular) = cellfun(@(x) x{1}(1),tokens(iRegular));
+bagNum(iX) = cellfun(@(x) x{1}(1),tokensX(iX));
 bagNum(iTransition) = cellfun(@(x) x{1}(1),tokensT(iTransition));
 bagNum = str2double(erase(bagNum,{'x','X','T',' '}));
 
@@ -54,19 +62,22 @@ bagDepthRange = repmat("",size(tokensT));
 bagDepthRange(iTransition) = cellfun(@(x) x{1}(2),tokensT(iTransition));
 bagTopDepths = nan(size(tokensT));
 bagTopDepths(iTransition) = str2double(extractBefore(bagDepthRange(iTransition),'-'));
-bagTopDepths(isnan(bagTopDepths) & iTransition) = 0;
+bagTopDepths(isnan(bagTopDepths) & iTransition) = 0; % Some transition samples have no depth range info included. Assume these samples were cut from the top (z = 0 cm).
 
 % Assign Depths
 % N.B. The Danes report Top Depths for NEEM that are just the sum of the
-% number of 55 cm bags that have come before. The SIO piece seems to always
+% number of 55 cm bags that have come before. The SIO piece seems to often
 % comprise the top 5 or 10 cm of each bag (unless noted, e.g. for the 
-% transition pieces). The bags for the Transition samples were renumbered
-% starting from bag 2580, which has a top depth of 1418.85 m.
+% transition pieces). 
+% The Transition samples were renumbered such that T1 = Bag 2580, which has
+% a top depth of 1418.85 m (see BigSampleBook_16112011_bestguessTB.xls).
+% The X-labelled samples were renumbered such that X228 = Bag 3471, which
+% has a top depth of 1908.5 m (see BigSampleBook_16112011_bestguessTB.xls).
 topDepth = nan(size(bagNum));
 topDepth(iRegular) = (bagNum(iRegular)-1)*0.55;
+topDepth(iX) = (bagNum(iX)-228)*0.55 + 1908.5;
 topDepth(iTransition) = 1418.45 + (bagNum(iTransition)-1)*0.55 + bagTopDepths(iTransition)/100;
-topDepth = topDepth(iNEEM);
 
 
 % Generate Ice Core Dataset
-neem = makeIceCoreDataset(neemMasterSheet,iNEEM,topDepth);
+neem = makeIceCoreDataset(neemMasterSheet,iNEEM,topDepth(iNEEM));
